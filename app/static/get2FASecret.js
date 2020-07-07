757c681b0c2a4fe23a947f345dbdb5bdea0a5a44
onyxcherry <tomek@onyxcherry.pl>
2020-07-07 10:28:54 +0200

    Add language support for client-side

--- app/static/get2FASecret.js
+++ app/static/get2FASecret.js
@@ -1,45 +1,81 @@
-const userLang = window.navigator.language
-const pl = ['pl', 'pl-PL']
+const userLang = window.navigator.language.split('-')[0]
+
+const base_url = 'http://127.0.0.1:5777'
 
 const panel_info = document.querySelector('.panel-info')
 const panel_danger = document.querySelector('.panel-danger')
 const panel_success = document.querySelector('.panel-success')
 const panel_primary = document.querySelector('.panel-primary')
+const panel_warning = document.querySelector('.panel-warning')
+
+messages = {
+    "otp_info":{
+       "en":[
+          "Scan the QR and type the code from app below.",
+          "Use an OTP app, e.g. Authy or Google Authenticator."
+       ],
+       "pl":[
+          "Zeskanuj poniższy kod QR.",
+          "Użyj aplikacji - np. Authy lub Google Authenticator."
+       ]
+    }
+ }
+
+
+view_as_text_button = document.getElementById('viewAsText')
+view_as_qr_button = document.getElementById('viewAsQR')
+
+twofa_qrcode = document.getElementById('2fa-qrcode')
+
+view_as_text_button.addEventListener('click', () => {
+    showAsText()
+})
+
+view_as_qr_button.addEventListener('click', () => {
+    showAsQR()
+})
+
+get_token_button = document.getElementById('getTokenButton')
+get_token_button.addEventListener('click', () => {
+    getToken();
+})
+
+check_code_form = document.getElementById('checkCode')
+check_code_form.addEventListener('submit', e => {
+    submitForm(e, check_code_form)
+})
 
 function invalidCode() {
-    panel_danger.firstElementChild.innerText = "Błędny kod OTP!"
-    panel_danger.children[1].innerText = "Spróbuj ponownie."
-    panel_danger.style.display = "block";
+    panel_warning.style.display = 'block';
 };
 
-function changeSecretView() {
-    if (document.getElementById('2fa-qrcode').style.display == 'block') {
-        document.getElementById('2fa-qrcode').style.display = 'none';
-        panel_primary.style.display = "block";
-        document.getElementById('changeview').innerText = "Show QR Code"
-        panel_info.style.display = "none"
-    } else {
-        document.getElementById('2fa-qrcode').style.display = 'block';
-        panel_primary.style.display = "none";
-        document.getElementById('changeview').innerText = "Show secret as text"
-        panel_info.style.display = "block"
-    }
+function showAsText() {
+    view_as_text_button.style.display = 'none'
+    view_as_qr_button.style.display = 'block'
+    twofa_qrcode.style.display = 'none'
+    panel_primary.style.display = 'block'
 }
 
+function showAsQR() {
+    view_as_text_button.style.display = 'block'
+    view_as_qr_button.style.display = 'none'
+    twofa_qrcode.style.display = 'block'
+    panel_primary.style.display = 'none'
+}
 
 function submitForm(e, form) {
     e.preventDefault();
-    fetch('http://127.0.0.1:5777/auth/checkcode', {
+    fetch(base_url + '/auth/checkcode', {
         method: 'post',
         headers: {
-            "Content-Type": "text/plain"
+            'Content-Type': 'text/plain'
         },
         body: form.user_code.value
     }).then(response => {
         if (response.ok) {
             return response.text()
         } else {
-            errorDiv();
+            errorDiv('rejecta reponse dostalem');
             return Promise.reject(response)
         }
     }).then(response => {
@@ -48,47 +84,37 @@ function submitForm(e, form) {
         } else {
             document.getElementById('2fa-user-code').style.display = 'none';
             document.getElementById('2fa-qrcode').style.display = 'none';
-            panel_success.style.display = "block"
-            panel_info.style.display = "none";
-            panel_danger.style.display = "none"
-            panel_success.firstElementChild.innerText = "Turned on OTP."
-            panel_success.children[1].innerText = "You could go to the main page."
+            panel_success.style.display = 'block'
+            panel_info.style.display = 'none'
+            panel_danger.style.display = 'none'
         }
-    }).catch(function(err) {
-        alert('Error')
+    }).catch(err => {
+        errorDiv('zlapalo blad')
     });
 }
 
-function errorDiv() {
-
-    panel_danger.style.display = "block";
-    if (pl.includes(userLang)) {
-        panel_danger.firstElementChild.innerText = "Wystąpił błąd."
-        panel_danger.children[1].innerText = "Skontaktuj się z administratorem serwisu."
-
-    } else {
-        panel_danger.firstElementChild.innerText = "An error occured."
-        panel_danger.children[1].innerText = "Please contact the administrator."
-    }
+function errorDiv(m) {
+    panel_danger.style.display = 'block';
+    console.log(m)
 }
 
 function getToken() {
-    fetch("http://127.0.0.1:5777/auth/generate_token")
+    fetch(base_url + '/auth/generate_token')
         .then(response => {
             if (response.ok) {
                 return response.json()
             } else {
-                errorDiv();
+                errorDiv('znowu reject');
                 return Promise.reject(response)
             }
         })
         .then(response => {
-            panel_info.firstElementChild.innerText = 'Scan the QR and type the code from app below.'
-            panel_info.children[1].innerText = 'Use an OTP app, e.g. Authy or Google Authenticator.'
+            panel_info.firstElementChild.innerText = messages['otp_info'][userLang][0]
+            panel_info.children[1].innerText = messages['otp_info'][userLang][1]
             if (response['app_qrcode']) {
                 var svgNode = QRCode(response['app_qrcode'])
             } else {
-                errorDiv()
+                errorDiv('cos nie moze wyswietlic')
             }
             if (response['secret']) {
                 panel_primary.innerText = response['secret']
@@ -102,6 +128,6 @@ function getToken() {
 
         })
         .catch(error => {
-            errorDiv();
+            errorDiv('kolejny zlapany blad');
         });
 }
