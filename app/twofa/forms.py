2854530d3b2a1f0cf923d965ccec07daf0038714
Tomek <tomek@onyxcherry.pl>
2020-09-20 14:34:44 +0200

    Add missed requirements, add OTP code max length validator

--- app/twofa/forms.py
+++ app/twofa/forms.py
@@ -1,9 +1,11 @@
 from flask_babel import lazy_gettext as _l
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class CheckOTPCode(FlaskForm):
-    otp_code = StringField(_l("Code: "), validators=[DataRequired()])
+    otp_code = StringField(
+        _l("Code: "), validators=[DataRequired(), Length(max=6)]
+    )
     submit = SubmitField(_l("Submit"))
